0e23096a40a7a6e317957248aaa5662ec4cf15af
dina <leshlesh1337@gmail.com>
2024-10-22 19:46:10 +0300

    added jwt auth

--- src/main/java/org/example/springbootcarservice/controllers/CarsController.java
+++ src/main/java/org/example/springbootcarservice/controllers/CarsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController // Data Access Object DAO
+@RestController // Data Access Object
 // API (REST http) = a way to interact with our application
 
 @RequestMapping("/cars")
